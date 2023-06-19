6866be78da128ba12296d75ca20812eabbd58323
Rifky Galuh <rfqgal@gmail.com>
2023-06-19 21:40:47 +0700

    tidy up codes and database

--- daftar.php
+++ daftar.php
@@ -2,33 +2,35 @@
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
     <meta charset="utf-8">
     <title>Form Input Data Siswa Baru</title>
     <h1>DATA SISWA BARU</h1>
 
 </head>
+
 <body>
     <form class="" action="proses.php" method="post">
         <p>
-        <label for="nama">Nama: </label>
-        <input type="text" name="nama" placeholder="nama lengkap" />
+            <label for="nama">Nama: </label>
+            <input type="text" name="nama" placeholder="nama lengkap" />
         </p>
         <p>
-        <label for="nik">NIK:</label>
-        <input type="text" name="nik" placeholder="nik harus 12 digit"/>
+            <label for="nik">NIK:</label>
+            <input type="text" name="nik" placeholder="nik harus 12 digit" />
         </p>
         <p>
-        <label for="temp_lahir">Tempat Lahir:</label>
-        <input type="text" name="temp_lahir" placeholder="tempat lahir"/>
+            <label for="tempat_lahir">Tempat Lahir:</label>
+            <input type="text" name="tempat_lahir" placeholder="tempat lahir" />
         </p>
         <p>
-        <label for="tgl_lahir">Tanggal Lahir:</label>
-        <input type="date" name="tgl_lahir"/>
+            <label for="tanggal_lahir">Tanggal Lahir:</label>
+            <input type="date" name="tanggal_lahir" />
         </p>
         <p>
             <label for="nisn">NISN:</label>
-            <input type="text" name="nisn" placeholder="nisn 10 digit"/>
+            <input type="text" name="nisn" placeholder="nisn 10 digit" />
         </p>
         <p>
             <label for="jenis_kelamin">Jenis Kelamin: </label>
@@ -50,85 +52,92 @@
             <input type="text" name="sekolah_asal" placeholder="nama sekolah asal" />
         </p>
         <p>
-            <label for="no_tlp">No Telepon:</label>
-            <input type="text" name="no_tlp" placeholder="nomor telepon aktif"/>
-        </p>
-        <p>
-            <label for="ibu">Nama Ibu:</label>
-            <input type="text" name="ibu" placeholder="nama ibu kandung"/>
-        </p>
-        <p>
-            <label for="tempat_ibu">Tempat Lahir:</label>
-            <input type="text" name="tempat_ibu" placeholder="tempat lahir ibu"/>
-        </p>
-        <p>
-            <label for="tgl_ibu">Tanggal Lahir:</label>
-            <input type="date" name="tgl_ibu"/> 
-        </p>
-        <p>
-            <label for="pend_ibu">Pendidikan Terakhir Ibu:</label>
-            <select name="pend_ibu">
-            <option>SD/Sederajat</option>
-            <option>SMP/Sederajat</option>
-            <option>SMA/Sederajat</option>
-            <option>D1</option>
-            <option>D2</option>
-            <option>D3</option>
-            <option>S1</option>
-            <option>S2</option>
-            <option>Tidak Sekolah</option>
-</select>
-        </P>
-        <p>
-            <label for="pekerj_ibu">Pekerjaan:</label>
-            <input type="text" name="pekerj_ibu" placeholder="pekerjaan ibu"/>
-        </p>
-        <p>
-            <label for="no_ibu">No Telepon:</label>
-            <input type="text" name="no_ibu" placeholder="nomor telepon ibu"/>
-        </p>
-        <p>
-            <label for="ayah">Nama Ayah:</label>
-            <input type="text" name="ayah" placeholder="nama lengkap ayah"/>
-        </P>
-        <p>
-            <label for="temp_ayah">Tempat Lahir:</label>
-            <input type="text" name="temp_ayah" placeholder="tempat lahir ayah"/>
-        </P>
-        <p>
-            <label for="tgl_ayah">Tanggal Lahir:</label>
-            <input type="date" name="tgl_ayah"/>
-        </P>
-        <P>
-            <label for="pend_ayah">Pendidikan Terakhir Ayah:</label>
-            <select name="pend_ayah">
-            <option>SD/Sederajat</option>
-            <option>SMP/Sederajat</option>
-            <option>SMA/Sederajat</option>
-            <option>D1</option>
-            <option>D2</option>
-            <option>D3</option>
-            <option>S1</option>
-            <option>S2</option>
-            <option>Tidak Sekolah</option>
-</select>
-        </p>
-        <p>
-            <label for=pekerj_ayah>Pekerjaan:</label>
-            <input type="text" name="pekerj_ayah" placeholder="pekerjaan ayah"/>
-        </P>
-        <p>
-            <label for="no_ayah">No Telepon:</label>
-            <input type="text" name="no_ayah" placeholder="no telepon ayah"/>
-        </p>
+            <label for="telepon">No Telepon:</label>
+            <input type="text" name="telepon" placeholder="nomor telepon aktif" />
         </p>
         <p>
             <label for="alamat">Alamat: </label>
             <textarea name="alamat"></textarea>
         </p>
         
-<br>
-<button type="submit" name="submit" value=<?php echo date("h:i:sa");?> >Simpan</button> <a href="index.php">Batal</a>
+        <br>
+        <p>
+            <label for="nama_ibu">Nama Ibu:</label>
+            <input type="text" name="nama_ibu" placeholder="nama ibu kandung" />
+        </p>
+        <p>
+            <label for="tempat_lahir_ibu">Tempat Lahir:</label>
+            <input type="text" name="tempat_lahir_ibu" placeholder="tempat lahir ibu" />
+        </p>
+        <p>
+            <label for="tanggal_lahir_ibu">Tanggal Lahir:</label>
+            <input type="date" name="tanggal_lahir_ibu" />
+        </p>
+        <p>
+            <label for="pendidikan_terakhir_ibu">Pendidikan Terakhir Ibu:</label>
+            <select name="pendidikan_terakhir_ibu">
+                <option>SD/Sederajat</option>
+                <option>SMP/Sederajat</option>
+                <option>SMA/Sederajat</option>
+                <option>D1</option>
+                <option>D2</option>
+                <option>D3</option>
+                <option>S1</option>
+                <option>S2</option>
+                <option>S3</option>
+                <option>Tidak Sekolah</option>
+            </select>
+        </P>
+        <p>
+            <label for="pekerjaan_ibu">Pekerjaan:</label>
+            <input type="text" name="pekerjaan_ibu" placeholder="pekerjaan ibu" />
+        </p>
+        <p>
+            <label for="telepon_ibu">No Telepon:</label>
+            <input type="text" name="telepon_ibu" placeholder="nomor telepon ibu" />
+        </p>
+
+        <br>
+        <p>
+            <label for="nama_ayah">Nama Ayah:</label>
+            <input type="text" name="nama_ayah" placeholder="nama lengkap ayah" />
+        </P>
+        <p>
+            <label for="tempat_lahir_ayah">Tempat Lahir:</label>
+            <input type="text" name="tempat_lahir_ayah" placeholder="tempat lahir ayah" />
+        </P>
+        <p>
+            <label for="tanggal_lahir_ayah">Tanggal Lahir:</label>
+            <input type="date" name="tanggal_lahir_ayah" />
+        </P>
+        <P>
+            <label for="pendidikan_terakhir_ayah">Pendidikan Terakhir Ayah:</label>
+            <select name="pendidikan_terakhir_ayah">
+                <option>SD/Sederajat</option>
+                <option>SMP/Sederajat</option>
+                <option>SMA/Sederajat</option>
+                <option>D1</option>
+                <option>D2</option>
+                <option>D3</option>
+                <option>S1</option>
+                <option>S2</option>
+                <option>S3</option>
+                <option>Tidak Sekolah</option>
+            </select>
+        </p>
+        <p>
+            <label for=pekerjaan_ayah>Pekerjaan:</label>
+            <input type="text" name="pekerjaan_ayah" placeholder="pekerjaan ayah" />
+        </P>
+        <p>
+            <label for="telepon_ayah">No Telepon:</label>
+            <input type="text" name="telepon_ayah" placeholder="no telepon ayah" />
+        </p>
+        </p>
+
+        <br>
+        <button type="submit" name="submit" value=<?php echo date("h:i:sa"); ?> >Simpan</button>
+        <a href="index.php">Batal</a>
 
 
     </form>
--- proses.php
+++ proses.php
@@ -3,33 +3,38 @@ $koneksi = mysqli_connect("localhost","root","","db_siswa");
 
 $nama = $_POST['nama'];
 $nik = $_POST['nik'];
-$tempat_lahir = $_POST['temp_lahir'];
-$tgl_lahir = $_POST['tgl_lahir'];
+$tempat_lahir = $_POST['tempat_lahir'];
+$tanggal_lahir = $_POST['tanggal_lahir'];
 $nisn = $_POST['nisn'];
 $jenis_kelamin = $_POST['jenis_kelamin'];
 $agama = $_POST['agama'];
-$sekolah = $_POST['sekolah_asal'];
-$no_telepon = $_POST['no_tlp'];
-$nama_ibu = $_POST['ibu'];
-$tempat_lahir_ibu = $_POST['tempat_ibu'];
-$tgl_lahir_ibu = $_POST['tgl_ibu'];
-$pendidikan_terakhir_ibu = $_POST['pend_ibu'];
-$pekerjaan_ibu = $_POST['pekerj_ibu'];
-$no_telepon_ibu = $_POST['no_ibu'];
-$tempat_lahir_ayah = $_POST['temp_ayah'];
-$tanggal_lahir_ayah = $_POST['tgl_ayah'];
-$pendidikan_terakhir_ayah = $_POST['pend_ayah'];
-$nama_ayah = $_POST['ayah'];
-$pekerjaan_ayah = $_POST['pekerj_ayah'];
-$no_telepon_ayah = $_POST['no_ayah'];
+$sekolah_asal = $_POST['sekolah_asal'];
+$telepon = $_POST['telepon'];
 $alamat = $_POST['alamat'];
 
+$nama_ibu = $_POST['nama_ibu'];
+$tempat_lahir_ibu = $_POST['tempat_lahir_ibu'];
+$tanggal_lahir_ibu = $_POST['tanggal_lahir_ibu'];
+$pendidikan_terakhir_ibu = $_POST['pendidikan_terakhir_ibu'];
+$pekerjaan_ibu = $_POST['pekerjaan_ibu'];
+$telepon_ibu = $_POST['telepon_ibu'];
+
+$nama_ayah = $_POST['nama_ayah'];
+$tempat_lahir_ayah = $_POST['tempat_lahir_ayah'];
+$tanggal_lahir_ayah = $_POST['tanggal_lahir_ayah'];
+$pendidikan_terakhir_ayah = $_POST['pendidikan_terakhir_ayah'];
+$pekerjaan_ayah = $_POST['pekerjaan_ayah'];
+$telepon_ayah = $_POST['telepon_ayah'];
+
 $query = "INSERT INTO data_siswa VALUES(
-    '$nama','$nik','$tempat_lahir','$tgl_lahir','$nisn','$jenis_kelamin',
-    '$agama','$sekolah','$no_telepon','$nama_ibu','$tempat_lahir_ibu','$tgl_lahir_ibu',
-    '$pendidikan_terakhir_ibu','$pekerjaan_ibu','$no_telepon_ibu','$nama_ayah',
-    '$tempat_lahir_ayah','$tanggal_lahir_ayah','$pendidikan_terakhir_ayah','$pekerjaan_ayah',
-    '$no_telepon_ayah','$alamat'
+    '$nama','$nik','$tempat_lahir','$tanggal_lahir','$nisn',
+    '$jenis_kelamin','$agama','$sekolah_asal','$telepon','$alamat',
+
+    '$nama_ibu','$tempat_lahir_ibu','$tanggal_lahir_ibu',
+    '$pendidikan_terakhir_ibu','$pekerjaan_ibu','$telepon_ibu',
+    
+    '$nama_ayah','$tempat_lahir_ayah','$tanggal_lahir_ayah',
+    '$pendidikan_terakhir_ayah','$pekerjaan_ayah','$telepon_ayah'
 )";
 
 mysqli_query($koneksi, $query);
